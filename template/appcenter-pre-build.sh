2faaf4d2b4614b7fe653b9921b403736f4ef4581
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2021-12-13 19:51:14 -0300

    add appcenter pre-build script

--- template/appcenter-pre-build.sh
+++ template/appcenter-pre-build.sh
@@ -1,5 +1,5 @@
-echo '{
-  "API": "$API",
-  "API_KEY": "$API_KEY",
-  "APP_VERSION": "$APP_VERSION"
-  }' > env.json
+#!/bin/bash
+cd ${APPCENTER_SOURCE_DIRECTORY}
+echo “API=${API}” > .env
+echo “API_KEY=${API_KEY}” >> .env
+echo “APP_VERSION=${APP_VERSION}” >> .env
