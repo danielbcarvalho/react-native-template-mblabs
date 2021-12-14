a429b2674b25c509d2693e300d91cac95d7a0a3e
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2021-12-14 11:52:33 -0300

    new enviroment variable test

--- template/appcenter-pre-build.sh
+++ template/appcenter-pre-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd template
-echo “API=${API}” > .env
-echo “API_KEY=${API_KEY}” >> .env
-echo “APP_VERSION=${APP_VERSION}” >> .env
+echo API=${API} > .env
+echo API_KEY=${API_KEY} >> .env
+echo APP_VERSION=${APP_VERSION} >> .env
